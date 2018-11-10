1ce76437a7334b3941dd4dade0c56d0067cdc486
Thomas <sauvage.thomas@gmail.com>
2018-11-10 14:46:28 +0100

    abiant luminosity

--- src/Salle.java
+++ src/Salle.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 import fr.irit.smac.amak.Environment;
 import fr.irit.smac.amak.Scheduling;
 
@@ -7,7 +9,7 @@ import fr.irit.smac.amak.Scheduling;
  *
  */
 public class Salle extends Environment {
-	public Salle(Object...params) {
+	public Salle(Object... params) {
 		super(Scheduling.DEFAULT, params);
 	}
 
@@ -23,7 +25,8 @@ public class Salle extends Environment {
 	 * Number of areas in height
 	 */
 	public final static int HEIGHT = 100;
-	
+
+	private int minutes;
 	private int hour;
 
 	/**
@@ -31,9 +34,9 @@ public class Salle extends Environment {
 	 */
 	@Override
 	public void onInitialization() {
-		
+
 		this.hour = 0;
-		
+
 		areas = new Area[HEIGHT][WIDTH];
 		for (int x = 0; x < WIDTH; x++) {
 			for (int y = 0; y < HEIGHT; y++) {
@@ -42,11 +45,56 @@ public class Salle extends Environment {
 		}
 	}
 
+	private float getAmbiantLuminosity() {
+		float l;
+		float delta = Math.abs(14 - this.hour);
+		if (delta >= 7) {
+			l = 0.2f;
+		} else {
+			l = delta / 7;
+		}
+		return l;
+	}
+	
+	private float getWindowLuminosity() {
+		float l;
+		Random r = new Random();
+		if(r.nextBoolean()) {
+			l = this.getAmbiantLuminosity()*0.8f+r.nextFloat()/5;
+		}else {
+			l = this.getAmbiantLuminosity()*0.8f-r.nextFloat()/5;
+		}
+		return l;
+	}
+	
+	private float getDoorLuminosity() {
+		float l;
+		Random r = new Random();
+		if(r.nextBoolean()) {
+			l = this.getAmbiantLuminosity()*0.25f+r.nextFloat()/5;
+		}else {
+			l = this.getAmbiantLuminosity()*0.25f-r.nextFloat()/5;
+		}
+		return l;
+	}
+
 	/**
-	 * Inform each area at each cycle
+	 * Inform each area at each cycle, 1 cycle = 1 minute
 	 */
 	@Override
 	public void onCycle() {
+
+		this.minutes += 1;
+		if (this.minutes == 60) {
+			this.hour += 1;
+			this.minutes = 0;
+		}
+		if (this.hour == 24) {
+			this.hour = 0;
+		}
+
+		System.out.println(this.hour + " heures et " + this.minutes + " minutes");
+
 		for (int x = 0; x < WIDTH; x++) {
 			for (int y = 0; y < HEIGHT; y++) {
 				areas[y][x].cycle();
@@ -66,10 +114,8 @@ public class Salle extends Environment {
 	/**
 	 * Get an area at a specific coordinate
 	 * 
-	 * @param dx
-	 *            the x coordinate
-	 * @param dy
-	 *            the y coordinate
+	 * @param dx the x coordinate
+	 * @param dy the y coordinate
 	 * @return the area
 	 */
 	public Area getAreaByPosition(int dx, int dy) {
